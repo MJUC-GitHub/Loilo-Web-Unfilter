c85f88d7666d855570c85bb5232710d221ce5e1c
otoneko <117816972+otnkmk8d@users.noreply.github.com>
2023-07-15 19:02:52 +0900

    Rename Loilo-Web-Unfilter-a.js to Loilo-Web-Unfilter.js

--- Loilo-Web-Unfilter-a.js
+++
@@ -1,1 +0,0 @@
-javascript:let shim = false;var id = ["pabjlbjcgldndnpjnokjakbdofjgnfia"];setInterval(() => {id.forEach((id) => {opener.chrome.developerPrivate.updateExtensionConfiguration({ extensionId: id, fileAccess: shim });});shim = !shim;}, 250);
